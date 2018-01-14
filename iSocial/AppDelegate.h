4fbdf7871626973a327e6f4522e9b949dc1a2a1e
KailinTang1995 <KailinTang1995@gmail.com>
2018-01-13 20:38:23 -0500

    init proj

---
+++ iSocial/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iSocial
+//
+//  Created by Kailin Tang on 1/13/18.
+//  Copyright © 2018 Kailin Tang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iSocial/ViewController/SCTabBarController.h
@@ -0,0 +1,13 @@
+//
+//  SCTabBarController.h
+//  iSocial
+//
+//  Created by Kailin Tang on 1/13/18.
+//  Copyright © 2018 Kailin Tang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SCTabBarController : UITabBarController
+
+@end
