173c23ddfba8acf37e51b29f780ff584f20e6988
KailinTang1995 <KailinTang1995@gmail.com>
2018-01-27 17:26:50 -0500

    Add SCLocationManager

---
+++ iSocial/Manager/SCLocationManager.h
@@ -0,0 +1,13 @@
+//
+//  SCLocationManager.h
+//  iSocial
+//
+//  Created by Kailin Tang on 1/27/18.
+//  Copyright © 2018 Kailin Tang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SCLocationManager : NSObject
+
+@end
